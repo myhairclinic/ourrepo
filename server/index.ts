f5f341e1c4d3323b185e2be0bab80cb731b0f5dd
myhairclinic <myhairclinictbilisi@gmail.com>
2025-05-13 18:03:42 +0300

    server ayarlamaları

--- server/index.ts
+++ server/index.ts
@@ -102,9 +102,8 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on port 5000
-  // this serves both the API and the client.
-  // It is the only port that is not firewalled.
+  // Railway deployment için process.env.PORT kullanılmalı
+  // Yoksa dönüş port için 5000 varsayılan değerini kullan
   const port = process.env.PORT || 5000;
   server.listen(port, () => {
     log(`serving on port ${port}`);
